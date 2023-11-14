3e8b31fbb23311e88d0871fb4f25f6f23d96b090
fourilent <fourilent@frlt.one>
2023-11-14 13:04:26 +0800

    Create homepage

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,18 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        "pop-in": "pop-in 0.4s ease-out",
+      },
+      keyframes: {
+        "pop-in": {
+          "0%": { transform: "scale(0.5)", opacity: 0 },
+          "75%": { transform: "scale(1.1)" },
+          "100%": { transform: "scale(1)", opacity: 1 },
+        },
+      },
+    },
   },
   plugins: [],
 };
