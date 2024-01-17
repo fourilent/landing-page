fb4bb7185d126f94c14f0a131c9208b5dfc31f66
fourilent <fourilent@frlt.one>
2024-01-17 13:17:25 +0800

    Add some CSP stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,12 @@ import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 import icon from "astro-icon";
 import Compress from "astro-compress";
+import GenerateCSPHash from "./src/util/csp-gen";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte(), icon(), Compress()],
+  build: {
+    inlineStylesheets: "never",
+  },
+  integrations: [tailwind(), svelte(), icon(), Compress(), GenerateCSPHash()],
 });
