33432dbd1f7c968579301754eeb513b2922cf395
fourilent <fourilent@frlt.one>
2024-01-17 11:08:15 +0800

    Add astro-compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,9 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 import icon from "astro-icon";
+import Compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte(), icon()],
+  integrations: [tailwind(), svelte(), icon(), Compress()],
 });
